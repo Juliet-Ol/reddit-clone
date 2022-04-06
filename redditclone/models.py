9ba39a42ebcec90d0993d6a6549b8750cdbb094a
Juliet <agutu@gmail.com>
2022-04-06 19:40:57 +0300

    create superuser and a model

--- redditclone/models.py
+++ redditclone/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    post = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    published_date = models.DateTimeField(auto_now_add=True)
+    # picture = CloudinaryField('image')
